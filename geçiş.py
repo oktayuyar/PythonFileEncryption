496f555cc10244da4e89112a687e33016308344f
oktayuyar <oktay-uyar@hotmail.com>
2016-03-25 00:46:10 +0200

    python formlar arası geçiş
    login ekranında şifreyi doğru girdiğimizde anasayfa açılıyor

--- geçiş.py
+++ geçiş.py
@@ -1,36 +1,61 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#directory encrypts
+
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 import time
+from PyQt5.QtCore import Qt
 
 from mainwindow import Ui_MainWindow
 from sifreG import Ui_Giris
 
+psw = '190323'
 
 
 class AltPen(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         self.setupUi(self)
+        self.move(300,150)
 
-        
 class AnaPen( QtWidgets.QMainWindow, Ui_Giris):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         self.setupUi(self)
         self.altpen = AltPen()
-        self.pushButton.clicked.connect(self.showw)
-        
-    def showw(self):
+        self.move(500,180)
+        self.btn.clicked.connect(self.girisYap)
+        self.clos.clicked.connect(self.kapat)
+        self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+    def girisYap(self):
+        if(self.sifre.text()==psw):
+            self.btn.clicked.connect(self.goster)
+        else:
+            QtWidgets.QMessageBox.information(self,"Bilgi","Şifreyi Yanlış Girdiniz.")
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Return or event.key() == Qt.Key_Enter:
+            if(self.sifre.text()==psw):
+                self.btn.clicked.connect(self.goster)
+            else:
+                QtWidgets.QMessageBox.information(self,"Bilgi","Şifreyi Yanlış Girdiniz.")
+        if event.key() == Qt.Key_Escape:
+            quit()
+
+    def kapat(self):
+            quit()
+
+    def goster(self):
         self.altpen.show()
-        time.sleep(0.1)
         ana.close()
 
-
-        
-    
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     ana = AnaPen()
